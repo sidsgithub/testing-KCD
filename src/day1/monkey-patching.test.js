fa9da62b94323ed1bea3d2c9cef38127f02c84ca
home <home@MacBook-Pro.local>
2021-05-26 00:33:43 +0530

    monkey patching

--- src/day1/monkey-patching.test.js
+++ src/day1/monkey-patching.test.js
@@ -1,6 +1,8 @@
 /*
 Monkey patching is a technique to add, modify, or suppress the default behavior of a piece of code
 at runtime without changing its original source code.
+
+Here, we intend to overwrite the function using monkey patching to mock it to  our requirements
 **/
 
 const { player } = require('../utils/player');
@@ -9,7 +11,7 @@ const songs = require('../utils/songs');
 const assert = require('assert')
 
 const originalShufflePlay = shuffle.shufflePlay; // storing the original method to clear afterwards
-shuffle.shufflePlay = () => songs[0]; // monkey patching shufflePlay function
+shuffle.shufflePlay = () => songs[0]; // monkey patching shufflePlay function  to always  return 'In the  End'
 
 test('song to be played : in the end',()=>{
     assert.strictEqual('In the End', player(1));
